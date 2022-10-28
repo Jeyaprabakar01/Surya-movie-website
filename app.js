b7b1e13c1cf7b6ff9ff22054591246d69542b309
Jeyaprabakar01 <ej.prabakar@gmail.com>
2022-10-28 16:23:20 +0530

    dark mode with JS

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ arrows.forEach((arrow, i) => {
     }
   });
 });
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.sidebar-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
